9996018bb71ee1c3cdc3601ff3672bf902d68bbf
abhiram-ar <abhiram2001.ars@gmail.com>
2026-07-12 17:46:19 +0000

    sum up n verbose

--- 13-loops/exercises/02-sum-the-numbers-verbose/main.go
+++ 13-loops/exercises/02-sum-the-numbers-verbose/main.go
@@ -8,6 +8,8 @@
 
 package main
 
+import "fmt"
+
 // ---------------------------------------------------------
 // EXERCISE: Sum the Numbers: Verbose Edition
 //
@@ -26,4 +28,15 @@ package main
 // ---------------------------------------------------------
 
 func main() {
+	sum := 0
+	for i := 1; i <= 10; i++ {
+		sum += i
+		fmt.Printf("%d", i)
+		if i != 10 {
+			fmt.Printf(" + ")
+		} else {
+			fmt.Printf(" = %d", sum)
+
+		}
+	}
 }
