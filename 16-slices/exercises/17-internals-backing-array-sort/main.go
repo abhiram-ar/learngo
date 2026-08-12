8443a63d3d3f1783927d44a3ed1fc32a44a6e65f
abhiram-ar <abhiram2001.ars@gmail.com>
2026-08-12 18:39:30 +0000

    sort middle elments

--- 16-slices/exercises/17-internals-backing-array-sort/main.go
+++ 16-slices/exercises/17-internals-backing-array-sort/main.go
@@ -10,6 +10,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 // ---------------------------------------------------------
@@ -49,6 +50,7 @@ func main() {
 
 	fmt.Println("Original:", items)
 	// ADD YOUR CODE HERE
+	sort.Strings(items[(len(items)/2)-1 : (len(items)/2)+1])
 	fmt.Println()
 	fmt.Println("Sorted  :", items)
 }
